3f2eaf7aa262d836597f37635cb92006f6e0b910
xooooooox <718337253@qq.com>
2019-12-25 15:57:31 +0800

    import import

--- rms.go
+++ rms.go
@@ -171,6 +171,7 @@ func WriteGoCode(table *sea.InformationSchemaTables) error {
 	code := "// Copyright (C) xooooooox\n\n"
 	code = fmt.Sprintf("%s// The following methods are only for single table operations\n\n", code)
 	code = fmt.Sprintf("%spackage "+PackageName+"\n\n", code)
+	code = fmt.Sprintf("%simport (\n\t\"githu.com/xooooooox/sea\"\n)\n\n", code)
 	code += fmt.Sprintf("// datetime %s\n\n", time.Now().Format("2006-01-02 15:04:05"))
 
 	//code = fmt.Sprintf("%simport (\n\t\"github.com/xooooooox/sea\"\n)\n\n", code)
